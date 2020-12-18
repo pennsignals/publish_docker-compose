c0237ba4e4c74051e44a124836b503ac37fe2a7c
Darryl Mendillo <1238655+darrylmendillo@users.noreply.github.com>
2020-12-18 12:19:08 -0500

    replace with Github ENV VARs

--- src/publish.sh
+++ src/publish.sh
@@ -1,7 +1,7 @@
 VERSION="$1"
 DOCKER_COMPOSE="$2"
 
-echo ${{ secrets.GITHUB_TOKEN }} | docker login docker.pkg.github.com -u ${{ github.actor }} --password-stdin
+echo ${{ secrets.GITHUB_TOKEN }} | docker login docker.pkg.github.com -u ${GITHUB_REF} --password-stdin
 
 # build and run the docker images
 docker-compose -f $DOCKER_COMPOSE up --no-start
@@ -15,8 +15,8 @@ while read -r IMAGE_ID; do
 
     echo "IMAGE_ID: $IMAGE_ID"
     # get the name label
-    NAME=$(basename ${{ github.repository }}).$(docker inspect --format '{{ index .Config.Labels.name }}' $IMAGE_ID)
-    PUSH="docker.pkg.github.com/${{ github.repository }}/$NAME:$VERSION"
+    NAME=$(basename ${GITHUB_REPOSITORY}).$(docker inspect --format '{{ index .Config.Labels.name }}' $IMAGE_ID)
+    PUSH="docker.pkg.github.com/${GITHUB_REPOSITORY}/$NAME:$VERSION"
 
     # tag and push
     docker tag $IMAGE_ID $PUSH
