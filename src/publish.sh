7b62e9fee57eca1b307b4d1735b25060c18e7d8c
Jason Lubken <jlubken@users.noreply.github.com>
2021-12-27 08:58:26 -0500

    Update publish.sh

--- src/publish.sh
+++ src/publish.sh
@@ -15,7 +15,7 @@ for IMAGE in $IMAGES; do
     echo "IMAGE: $IMAGE"
     
     NAME=$(basename ${GITHUB_REPOSITORY}).$(docker inspect --format '{{ index .Config.Labels "name" }}' $IMAGE)
-    TAG="ghrc.io/${GITHUB_REPOSITORY}/$NAME:$VERSION"
+    TAG="ghcr.io/${GITHUB_REPOSITORY}/$NAME:$VERSION"
 
     docker tag $IMAGE $TAG
     docker push $TAG
